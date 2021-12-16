80dc117b614c8679163c6a710d884672614fb19e
jspc <james+git@zero-internet.org.uk>
2021-12-16 12:57:56 +0000

    Point to gateway domain, serving files

--- main.go
+++ main.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"path/filepath"
 
 	"github.com/jspc/wg-sock-stats/stats"
+	"github.com/rs/cors"
 	"google.golang.org/grpc"
 	"google.golang.org/protobuf/encoding/protojson"
 )
@@ -23,7 +25,8 @@ func main() {
 
 	client := stats.NewStatsClient(conn)
 
-	http.HandleFunc("/stats", func(w http.ResponseWriter, r *http.Request) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/stats", func(w http.ResponseWriter, r *http.Request) {
 		d, err := client.Get(context.Background(), new(stats.Statistics))
 		if err != nil {
 			w.WriteHeader(http.StatusBadGateway)
@@ -35,5 +38,12 @@ func main() {
 		fmt.Fprint(w, string(body))
 	})
 
-	panic(http.ListenAndServe(":8080", nil))
+	// catchall media
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, filepath.Join("ui/build", r.URL.Path))
+	})
+
+	handler := cors.Default().Handler(mux)
+
+	panic(http.ListenAndServe(":8080", handler))
 }
